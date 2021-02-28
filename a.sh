45c090cb2ca95d2008acbf487779c9a7956f4bab
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-02-28 11:58:04 +0200

    a

--- a.sh
+++ a.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-echo "Welcome to a 2.0"
+echo "Welcome to a 2.1"
 echo "Press Ctrl+C to exit"
 
 while true
 do
+	i=$((i+1))
+	
+	if [ $i -gt 5000 ]
+		then
+			git push origin
+			i=0
+	fi
+	
 	echo "a" >> a
 	
 	git add . > /dev/null
